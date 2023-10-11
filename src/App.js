72c20325479fefcd3240f22841cd4f4953846f4f
Moises Elias Ramirez Cabrera <141798007+Sesiom31@users.noreply.github.com>
2023-10-10 23:48:57 -0500

    Manejo del estado

--- src/App.js
+++ src/App.js
@@ -6,13 +6,112 @@ import {
   faPowerOff,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./App.css";
+import alarmAudio from './assets/Alarm_Clock.mp3'
 
 function App() {
+  const [initialMinSession, setInitialMinSession] = useState(25);
+  const [initialMinBreak, setInitialMinBreak] = useState(5);
+  const [minutosSession, setMinutosSession] = useState(initialMinSession);
+  const [minutosBreak, setMinutosBreak] = useState(initialMinBreak);
+  const [segundos, setSegundos] = useState(0);
   const [isPlay, setIsPlay] = useState(false);
-  const [min, setMin] = useState(25);
-  const [seg, setSeg] = useState(0);
+  const [isSession, setIsSession] = useState(true);
+  const audioRef = useRef(null);
+
+  const minColor = minutosBreak < 1 || minutosSession < 1;
+
+  useEffect(() => {
+    let timer;
+    if (isPlay) {
+      timer = setInterval(() => {
+        if (segundos > 0) {
+          setSegundos(segundos - 1);
+        } else {
+          if (isSession && minutosSession === 0) {
+            audioRef.current.play()
+            setTimeout(() => {
+              setIsSession(false);
+              setMinutosSession(initialMinSession);
+            }, 1500);
+          } else if (isSession) {
+            setMinutosSession(minutosSession - 1);
+            setSegundos(59);
+          } else if (!isSession && minutosBreak === 0) {
+            audioRef.current.play()
+            setTimeout(() => {
+              setIsSession(true);
+              setMinutosBreak(initialMinBreak);
+            }, 1500);
+          } else if (!isSession) {
+            setMinutosBreak(minutosBreak - 1);
+            setSegundos(59);
+          }
+        }
+      }, 1000);
+    }
+
+    return () => clearInterval(timer);
+  }, [
+    isPlay,
+    segundos,
+    isSession,
+    minutosSession,
+    minutosBreak,
+    initialMinBreak,
+    initialMinSession,
+  ]);
+
+  const handleTogglePlay = () => {
+    setIsPlay(!isPlay);
+  };
+
+  const handleIncrementTimeSession = () => {
+    if (!isPlay) {
+      setInitialMinSession(initialMinSession < 60 ? initialMinSession + 1 : 60);
+      setMinutosSession(initialMinSession < 60 ? initialMinSession + 1 : 60);
+      setSegundos(isSession ? 0 : segundos);
+    }
+  };
+
+  const handleDecrementTimeSession = () => {
+    if (!isPlay) {
+      setInitialMinSession(initialMinSession > 1 ? initialMinSession - 1 : 1);
+      setMinutosSession(initialMinSession > 1 ? initialMinSession - 1 : 1);
+      setSegundos(isSession ? 0 : segundos);
+    }
+  };
+
+  const handleIncrementTimeBreak = () => {
+    if (!isPlay) {
+      setInitialMinBreak(initialMinBreak < 60 ? initialMinBreak + 1 : 60);
+      setMinutosBreak(initialMinBreak < 60 ? initialMinBreak + 1 : 60);
+      setSegundos(isSession ? segundos : 0);
+    }
+  };
+
+  const handleDecrementTimeBreak = () => {
+    if (!isPlay) {
+      setInitialMinBreak(initialMinBreak > 1 ? initialMinBreak - 1 : 1);
+      setMinutosBreak(initialMinBreak > 1 ? initialMinBreak - 1 : 1);
+      setSegundos(isSession ? segundos : 0);
+    }
+  };
+
+  const handleReset = () => {
+    setInitialMinSession(25);
+    setInitialMinBreak(5);
+    setMinutosSession(25);
+    setMinutosBreak(5);
+    setSegundos(0);
+    setIsPlay(false);
+    setIsSession(true);
+    audioRef.current.pause()
+    audioRef.current.currentTime = 0
+
+
+  };
 
   return (
     <div className="container">
@@ -22,11 +121,11 @@ function App() {
         <div className="controls left">
           <h3 id="break-label">Time Break</h3>
           <div className="control">
-            <button id="break-increment">
+            <button id="break-increment" onClick={handleIncrementTimeBreak}>
               <FontAwesomeIcon icon={faCircleArrowUp} />
             </button>
-            <span id="break-length">5</span>
-            <button id="break-decrement">
+            <span id="break-length">{initialMinBreak}</span>
+            <button id="break-decrement" onClick={handleDecrementTimeBreak}>
               <FontAwesomeIcon icon={faCircleArrowDown} />
             </button>
           </div>
@@ -35,11 +134,11 @@ function App() {
         <div className="controls right">
           <h3 id="session-label">Time Session</h3>
           <div className="control">
-            <button id="session-increment">
+            <button id="session-increment" onClick={handleIncrementTimeSession}>
               <FontAwesomeIcon icon={faCircleArrowUp} />
             </button>
-            <span id="session-length">25</span>
-            <button id="session-decrement">
+            <span id="session-length">{initialMinSession}</span>
+            <button id="session-decrement" onClick={handleDecrementTimeSession}>
               <FontAwesomeIcon icon={faCircleArrowDown} />
             </button>
           </div>
@@ -48,18 +147,34 @@ function App() {
 
       <div className="container-time">
         <div className="time">
-          <h3 id="timer-label">Session</h3>
-          <div id="time-left">
-            {min.toString().padStart(2, "0")}:{seg.toString().padStart(2, "0")}
+          <h3
+            id="timer-label"
+            style={{ color: minColor ? "#9c0720" : "inherit" }}
+          >
+            {isSession ? "Session" : "Break"}
+          </h3>
+          <div
+            id="time-left"
+            style={{ color: minColor ? "#9c0720" : "inherit" }}
+          >
+            {isSession
+              ? minutosSession.toString().padStart(2, "0")
+              : minutosBreak.toString().padStart(2, "0")}
+            :{segundos.toString().padStart(2, "0")}
           </div>
+          <audio ref={audioRef} src={alarmAudio} volume={50}></audio>
         </div>
 
         <div className="btns">
-          <button id="start_stop">
-            <FontAwesomeIcon icon={faPlay} /> <FontAwesomeIcon icon={faPause} />
+          <button id="start_stop" onClick={handleTogglePlay}>
+            {isPlay ? (
+              <FontAwesomeIcon icon={faPause} />
+            ) : (
+              <FontAwesomeIcon icon={faPlay} />
+            )}
           </button>
 
-          <button id="reset">
+          <button id="reset" onClick={handleReset}>
             <FontAwesomeIcon icon={faPowerOff} />
           </button>
         </div>
